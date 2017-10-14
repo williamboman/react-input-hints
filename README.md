9b6162934c2ab418a498bfa200ab6b9ffcfd893e
William Boman <william@redwill.se>
2017-10-15 00:11:56 +0200

    use react-typewriting

--- README.md
+++ README.md
@@ -7,63 +7,33 @@
 ## Usage
 
 ```jsx
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-ReactDOM.render(
-    <InputHints
-        placeholders={[
-            'Enter your username here...',
-            'Usernames can be 7-18 characters long.',
-        ]} />,
-    document.body
+<InputHints
+    placeholders={[
+        'Enter your username here...',
+        'Usernames can be 7-18 characters long.',
+    ]} />
 )
 ```
-
-## Component's ref API
-
-The component implements an API that makes it possible to interact with the DOM node itself. Use [`ref`](https://facebook.github.io/react/docs/refs-and-the-dom.html)s to access these methods.
-
-### `focus()`
-
-Brings focus on the input element.
-
-### `blur()`
-
-Removes focus from the input element.
 
 ## Installation
 
 ```sh
 $ npm install react-input-hints
+# or
+$ yarn add react-input-hints
 ```
 
 ## Props
 
-### `placeholders` | `array`
+*See [react-typewriting](https://github.com/williamboman/react-typewriting#props) for more controls.*
+
+### `placeholders` | `Array<string>` | *required*
 
 The placeholders to print out, in order of appearance.
-
-### `waitBeforeDeleteMs` | `number` | default: 2000
-
-Amount of milliseconds placeholders will be fully readable before starting
-to delete the placeholder.
-
-### `writeSpeedMs` | `number` | default: 100
-
-The absolute slowest speed to wait between printing characters (characters are printed at random intervals that span from 0 ms to whatever this config value is set to).
-
-### `deleteSpeedMs` | `number` | default: 60
-
-Same as `writeSpeedMs` (see above), but for when deleting characters.
 
 ## Tips
 
 Use the [`selector:placeholder`](http://css-tricks.com/snippets/css/style-placeholder-text/) CSS pseudo-class to style your placeholders!
-
-## Todos
-
-- Make it support arbitrary components via props.
 
 ## License
 
--- src/InputHints.js
+++ src/InputHints.js
@@ -1,167 +1,26 @@
 const React = require('react')
-const {Component} = React
+const {PureComponent} = React
 const PropTypes = require('prop-types')
+const Typewriting = require('react-typewriting')
 
-const TICK_INIT = 'TICK_INIT'
-const TICK_WRITE = 'TICK_WRITE'
-const TICK_DELETE = 'TICK_DELETE'
-const START_DELETE = 'START_DELETE'
-
-function getInputProps({...inputProps}) {
-    delete inputProps.placeholders
-    delete inputProps.waitBeforeDeleteMs
-    delete inputProps.writeSpeedMs
-    delete inputProps.deleteSpeedMs
-    delete inputProps.placeholder
-    return inputProps
-}
-
-class InputHints extends Component {
-
+export default class InputHints extends PureComponent {
     static propTypes = {
-        placeholders: PropTypes.array.isRequired,
-        waitBeforeDeleteMs: PropTypes.number,
-        writeSpeedMs: PropTypes.number,
-        deleteSpeedMs: PropTypes.number,
+        placeholders: PropTypes.arrayOf(PropTypes.string).isRequired,
     }
 
     static defaultProps = {
         waitBeforeDeleteMs: 2000,
-        writeSpeedMs: 100,
-        deleteSpeedMs: 60,
-    }
-
-    constructor(props) {
-        super(props)
-        this.tick = this.tick.bind(this)
-        this.state = {
-            currentPlaceholderIdx: 0,
-            currentCharPos: 0,
-            isDeleting: false,
-            inputProps: getInputProps(props),
-        }
-    }
-
-    componentDidMount() {
-        this.queueTick(TICK_INIT)
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this.ticker)
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            inputProps: getInputProps(nextProps),
-        })
-    }
-
-    randomizeTimeout(ms) {
-        // TODO: probably should implement a minimum timeout
-        return Math.random() * ms
-    }
-
-    queueTick(type) {
-        const {
-            writeSpeedMs,
-            deleteSpeedMs,
-            waitBeforeDeleteMs,
-        } = this.props
-
-        const timeout =
-            type === TICK_INIT ? 0 :
-            type === TICK_WRITE ? this.randomizeTimeout(writeSpeedMs) :
-            type === TICK_DELETE ? this.randomizeTimeout(deleteSpeedMs) :
-            type === START_DELETE ? waitBeforeDeleteMs :
-            0 // ¯\_(ツ)_/¯
-
-        this.ticker = setTimeout(this.tick, timeout)
-    }
-
-    moveToNextPlaceholder() {
-        const {placeholders} = this.props
-        const {currentPlaceholderIdx} = this.state
-        const nextPlaceholderIdx = currentPlaceholderIdx + 1
-        this.setState({
-            isDeleting: false,
-            currentPlaceholderIdx: nextPlaceholderIdx < placeholders.length ? nextPlaceholderIdx : 0,
-            currentCharPos: 0,
-        })
-    }
-
-    _getTextInputNode() {
-        return this.refTextInput
-    }
-
-    tick() {
-        const {placeholders} = this.props
-        const {
-            currentPlaceholderIdx,
-            currentCharPos,
-            isDeleting,
-        } = this.state
-
-        const currentPlaceholder = placeholders[currentPlaceholderIdx]
-
-        const nextCharPos = isDeleting
-            ? currentCharPos - 1
-            : currentCharPos + 1
-
-        if (isDeleting) {
-            if (nextCharPos < 0) {
-                this.moveToNextPlaceholder()
-            } else {
-                this.setState({
-                    currentCharPos: nextCharPos,
-                })
-            }
-            this.queueTick(TICK_DELETE)
-        } else {
-            if (nextCharPos > currentPlaceholder.length) {
-                this.setState({
-                    isDeleting: true,
-                })
-                this.queueTick(START_DELETE)
-            } else {
-                this.setState({
-                    currentCharPos: nextCharPos,
-                })
-                this.queueTick(TICK_WRITE)
-            }
-        }
     }
 
     render() {
-        const {placeholders} = this.props
-        const {
-            currentPlaceholderIdx,
-            currentCharPos,
-            inputProps,
-        } = this.state
-
-        const currentPlaceholder = placeholders[currentPlaceholderIdx]
-        const placeholder = currentPlaceholder.slice(0, currentCharPos)
-
+        const {placeholders, ...props} = this.props
         return (
-            <input ref={(e) => this.refTextInput = e} placeholder={placeholder} {...inputProps} />
+            <Typewriting
+                {...props}
+                component='input'
+                stringPropName='placeholder'
+                strings={placeholders}
+            />
         )
     }
-
 }
-
-function inputWrapper(WrappedComponent) {
-    class Input extends Component {
-        /* eslint-disable brace-style */
-        focus() { this.c._getTextInputNode().focus() }
-        blur() { this.c._getTextInputNode().blur() }
-        /* eslint-enable brace-style */
-
-        render() {
-            return <WrappedComponent {...this.props} ref={(c) => this.c = c} />
-        }
-    }
-    Input.displayName = WrappedComponent.displayName || 'Component'
-    return Input
-}
-
-module.exports = inputWrapper(InputHints)
